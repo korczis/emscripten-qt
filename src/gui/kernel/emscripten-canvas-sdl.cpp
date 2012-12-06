b066cd23673a20612acddf0e05b2dc2198a09a87
Simon St James <kdedevel@etotheipiplusone.com>
2012-12-06 10:10:56 +0000

    Now have Qt event loop, implemented in terms of SDL timers. Not happy with the API, yet - no way to set the screen dimensions before QApplication is called.

--- src/gui/kernel/emscripten-canvas-sdl.cpp
+++ src/gui/kernel/emscripten-canvas-sdl.cpp
@@ -8,14 +8,59 @@ namespace
 	int canvasWidthPixels = 800;
 	int canvasHeightPixels = 640;
 	SDL_Surface *sdlCanvas = NULL;
+	SDL_TimerID callbackTimer = NULL;
+	bool sdlInited = false;
 }
 
 extern "C" 
 {
+	Uint32 timerCallback(Uint32 interval, void *param)
+	{
+		qDebug() << "timerCallback";
+		SDL_Event event;
+		SDL_UserEvent userevent;
+
+		userevent.type = SDL_USEREVENT;
+		userevent.code = 0;
+		userevent.data1 = NULL;
+		userevent.data2 = NULL;
+
+		event.type = SDL_USEREVENT;
+		event.user = userevent;
+
+		SDL_PushEvent(&event);
+		return 0; // We don't want this timer to repeat, thanks!
+	}
+
 	void EMSCRIPTENQT_resetTimerCallback(long milliseconds)
 	{
-		qDebug() << "resetTimerCallback: " << milliseconds;
+		if (!sdlInited)
+		{	
+			// Can't set SDL timers before SDL is inited; ignore.
+			return;
+		}
+		if (callbackTimer)
+		{
+			SDL_RemoveTimer(callbackTimer);
+			callbackTimer = NULL;
+		}
+		if (milliseconds == 0)
+		{
+			milliseconds = 1;
+		}
+		callbackTimer = SDL_AddTimer(milliseconds, timerCallback, NULL);
+		if (callbackTimer == NULL)
+		{
+			qDebug() << "Error adding SDL timer: " << SDL_GetError();
+		}
+		else
+		{
+			qDebug() << "added sdl timer: " << callbackTimer;
+		}
 	}
+
+	void EMSCRIPTENQT_timerCallback();
+
 	int EMSCRIPTEN_canvas_width_pixels()
 	{
 		return canvasWidthPixels;
@@ -41,7 +86,7 @@ extern "C"
 				pos++;
 			}
 		}
-//		qDebug() << rgb;
+		//		qDebug() << rgb;
 		return 0;
 	}
 	void EMSCRIPTENQT_cursorChanged(int)
@@ -65,8 +110,12 @@ int EmscriptenSDL::exec(int canvasWidthPixels, int canvasHeightPixels)
 		qDebug() << "Creation of " << canvasWidthPixels << "x" << canvasHeightPixels << " SDL surface failed!";
 		return -1;
 	}
+	sdlInited = true;
 
 	qDebug() << "SDL - woo!";
+	// Any requested timers from before we called SDL_Init would have been ignored, so let's
+	// set up a synthetic one, now.
+	EMSCRIPTENQT_resetTimerCallback(0);
 
 	SDL_Event event;
 	bool quit = false;
@@ -78,6 +127,13 @@ int EmscriptenSDL::exec(int canvasWidthPixels, int canvasHeightPixels)
 			qDebug() << "Quitting";
 			quit = true;
 		}    
+		else if (event.type == SDL_USEREVENT)
+		{
+			// Qt timer.
+			qDebug() << "Calling Qt event loop";
+			EMSCRIPTENQT_timerCallback();
+		}
+
 	}
 	qDebug() << "Exiting SDL::exec";
 	return 0;

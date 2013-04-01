0b3abb305a519084f7c79d75ad30006317e843a1
Simon St James <kdedevel@etotheipiplusone.com>
2013-04-01 10:06:39 +0100

    Return QHtml5CanvasPixmapData from QHtml5CanvasGraphicsSystem::createPixmapData.

--- src/gui/painting/qgraphicssystem_html5canvas.cpp
+++ src/gui/painting/qgraphicssystem_html5canvas.cpp
@@ -1,5 +1,6 @@
 #include <qscreen_qws.h>
 #include "qgraphicssystem_html5canvas_p.h"
+#include <image/qpixmap_html5canvas_p.h>
 #include <private/qpixmap_raster_p.h>
 #include <private/qwindowsurface_qws_p.h>
 
@@ -7,17 +8,8 @@ QT_BEGIN_NAMESPACE
 
 QPixmapData *QHtml5CanvasGraphicsSystem::createPixmapData(QPixmapData::PixelType type) const
 {
-    qDebug() << "QHtml5CanvasGraphicsSystem::createPixmapData: " << type;
-    if (QScreen::instance()->pixmapDataFactory())
-    {
-        qDebug() << "Using pixmap factory";
-        return QScreen::instance()->pixmapDataFactory()->create(type); //### For 4.4 compatibility
-    }
-    else
-    {
-        qDebug() << "Using raster pixmap";
-        return new QRasterPixmapData(type);
-    }
+    qDebug() << "Returning QHtml5CanvasPixmapData";
+    return new QHtml5CanvasPixmapData(type);
 }
 
 QWindowSurface *QHtml5CanvasGraphicsSystem::createWindowSurface(QWidget *widget) const

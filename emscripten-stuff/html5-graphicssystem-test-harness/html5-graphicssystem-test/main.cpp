4b2873a9bc47a1ffba7cdbda144a2db04322066e
Simon St James <kdedevel@etotheipiplusone.com>
2013-04-17 14:33:24 +0100

    Flush the qDebug() message output straight away.

--- emscripten-stuff/html5-graphicssystem-test-harness/html5-graphicssystem-test/main.cpp
+++ emscripten-stuff/html5-graphicssystem-test-harness/html5-graphicssystem-test/main.cpp
@@ -10,6 +10,7 @@ void myMessageOutput(QtMsgType type, const char *msg)
  switch (type) {
  case QtDebugMsg:
      fprintf(stdout, "%s\n", msg);
+     fflush(stdout);
      break;
  case QtWarningMsg:
      fprintf(stderr, "Warning: %s\n", msg);
